8302baefa73ec03dbfe9abc2dae9fba1548d7379
Dmitriy Kosyakov <fatnlazycat@gmail.com>
2022-07-09 21:21:55 +0200

    days 2-3

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import 'reflect-metadata';
 import { AppDataSource } from './src/data-source';
 import { Note } from './src/entity/note';
@@ -14,6 +15,14 @@ AppDataSource.initialize()
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(express.json());
+
+app.all('/*', (req: Request, res: Response, next) => {
+  console.log('request=', req.body);
+  next();
+});
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
@@ -24,9 +33,16 @@ app.get('/notes', async (req: Request, res: Response) => {
 });
 
 app.post('/notes', async (req: Request, res: Response) => {
-  const note = req.body as Note;
-  const saved = await AppDataSource.getRepository(Note).save(note);
-  res.status(201).send(saved);
+  console.log('POST, req=', req.params);
+  try {
+    const note = req.body as Note;
+    const saved = await AppDataSource.getRepository(Note).save(note);
+    console.log('saved with save=', saved);
+    res.status(201).send(saved);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 });
 
 app.patch('/notes', async (req: Request, res: Response) => {
@@ -37,12 +53,15 @@ app.patch('/notes', async (req: Request, res: Response) => {
 
 app.delete('/notes/:noteId', async (req: Request, res: Response) => {
   const noteId = Number(req.params.noteId);
-  if (noteId) {
-    const saved = await AppDataSource.getRepository(Note).findOneBy({ id: noteId });
-    res.status(201).send(saved);
-  } else {
-    res.status(404).send();
+  const toDelete = await AppDataSource.getRepository(Note).findOneBy({ id: noteId });
+  if (toDelete) {
+    const deleted = await AppDataSource.getRepository(Note).delete(toDelete);
+    if (deleted.affected === 1) {
+      res.status(200).send(toDelete);
+      return;
+    }
   }
+  res.status(404).send();
 });
 
 app.listen(port, () => {
